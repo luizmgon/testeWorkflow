309cb9a74c0a469e779007f05a39299e88139ff8
Luiz Henrique Marques Gonçalves <luizmg03@gmail.com>
2023-04-25 22:31:59 -0300

    dsr

--- script.py
+++ script.py
@@ -8,12 +8,12 @@ print(os.listdir())
 csv_files = os.listdir('CSV Files')
 print(csv_files)
 
-with open('CSV Files\\added_csvs.txt') as f:
+with open('CSV Files/added_csvs.txt') as f:
     added = f.readlines()
 
 for file in csv_files:
     if((file+'\n') not in added):
-        with open('CSV Files\\added_csvs.txt', 'a') as f:
+        with open('CSV Files/added_csvs.txt', 'a') as f:
             f.write(file + '\n')
         corrige_csv(file)
         
