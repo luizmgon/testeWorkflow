d170bf33096aff7edd059be26fb1268c3b886ec5
Luiz Henrique Marques Gonçalves <luizmg03@gmail.com>
2023-04-25 21:51:39 -0300

    adding base files

---
+++ Script/script.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import os
+
+def corrige_csv(nome_do_arquivo_entrada):
+    print('O arquivo ' + nome_do_arquivo_entrada+ ' foi upado.')
+
+csv_files = os.listdir('CSV Files')
+
+with open('CSV Files\\added_csvs.txt') as f:
+    added = f.readlines()
+
+for file in csv_files:
+    if((file+'\n') not in added):
+        with open('CSV Files\\added_csvs.txt', 'a') as f:
+            f.write(file + '\n')
+        corrige_csv(file)
+        
+
